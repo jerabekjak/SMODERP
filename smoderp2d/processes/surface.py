d353f4ded29fa69d4e54071a1e9a1b79dec18583
jerabekjak <jerabekjak@fsv.cvut.cz>
2018-04-27 10:58:19 +0200

    rm src.

---
+++ smoderp2d/processes/surface.py
@@ -0,0 +1,12 @@
+import math
+from smoderp2d.src.main_classes.General import Globals as Gl
+
+
+def shallowSurfaceKinematic(i,j,sur):
+
+    a = Gl.mat_aa[i][j]
+    b = Gl.mat_b[i][j]
+    #print sur.h_total_pre, sur.h_crit
+    h = min(sur.h_total_pre,sur.h_crit) 
+
+    return math.pow(h, b) * a
