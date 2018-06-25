33fbdb7097201c15538f60a6a7f1dba0ac7d6339
jerabekjak <jerabekjak@email.cz>
2018-06-25 17:44:16 +0200

    X and Y in config instead of A

--- smoderp2d/tools/resolve_partial_computing.py
+++ smoderp2d/tools/resolve_partial_computing.py
@@ -99,7 +99,12 @@ def get_indata_lin(tc, args):
             # os.makedirs(output+os.sep+'prubeh')
         
         # change parametrs of selected matrices for sensitivity analysis
-        mat_aa.fill(Config.getfloat('citlivost', 'A'))
+        
+        mat_X = mat_aa.copy()
+        mat_X.fill(Config.getfloat('citlivost', 'X'))
+        mat_Y = mat_aa.copy()
+        mat_Y.fill(Config.getfloat('citlivost', 'Y'))
+        mat_aa = mat_X*mat_slope**mat_Y
         mat_b.fill(Config.getfloat('citlivost',  'b'))
         mat_n.fill(Config.getfloat('citlivost',  'n'))
         
